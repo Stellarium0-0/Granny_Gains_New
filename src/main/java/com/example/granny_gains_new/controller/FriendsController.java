4ef44bf5dd9ae46f645c7787f67f49f44f4032fd
eFreedo <ethanfriedmanqld@gmail.com>
2024-10-10 19:16:57 +1000

    updated friends UI - dynamic list + adding input to list. also deleted add_friends_page (no longer needed)

--- src/main/java/com/example/granny_gains_new/controller/FriendsController.java
+++ src/main/java/com/example/granny_gains_new/controller/FriendsController.java
@@ -5,6 +5,8 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.control.TitledPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -21,6 +23,10 @@ public class FriendsController {
     private TextField AddFriend;
 
     @FXML
+    private VBox friendsVBox;
+
+    // to home page
+    @FXML
     protected void handleBackToHome() throws IOException {
         Stage stage = (Stage) BackButton.getScene().getWindow();
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/com/example/granny_gains_new/granny_gains_home.fxml"));
@@ -29,15 +35,29 @@ public class FriendsController {
     }
 
 
-@FXML
-protected void handleToAddFriend() throws IOException {
-    Stage stage = (Stage) AddFriendButton.getScene().getWindow();
-    FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/com/example/granny_gains_new/add_friends_page.fxml"));
-    Scene scene = new Scene(fxmlLoader.load(), 1000, 1000);
-    stage.setScene(scene);
+
+    @FXML
+    public void initialize() {
+        AddFriendButton.setOnAction(event -> addFriend());
+    }
+
+    // VBOX
+    private void addFriend() {
+        String friendName = AddFriend.getText();
+
+        // Only add if text is not null
+        if (friendName != null && !friendName.trim().isEmpty()) {
+            // Create a pane
+            TitledPane newFriendPane = new TitledPane();
+            newFriendPane.setText(friendName);
+            newFriendPane.setPrefHeight(26.0);
+            newFriendPane.setPrefWidth(333.0);
+            newFriendPane.setStyle("-fx-background-color: #D1FF98; -fx-border-color: #818589;");
+
+            // Add input to box
+            friendsVBox.getChildren().add(newFriendPane);
+
+            AddFriend.clear();
+        }
+    }
 }
-}
-
-
-
-
